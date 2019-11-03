1e0f2f318813b4b288d60d7dc7f66c540f78480a
Owen David <1999yaipen@gmail.com>
2019-11-03 18:15:33 -0500

    main page

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import jQuery from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './sass/app.scss';
+
+window.Popper = Popper
+window.$ = window.jQuery = jQuery;
+
+
+//Components
+Vue.component('Navbar', require('./components/Navbar.vue').default);
 
 Vue.config.productionTip = false
 
