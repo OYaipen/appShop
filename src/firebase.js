6e3c792e0cd832d6ce435c9eeddcff22b20cfcdb
Owen Yaipen <1999yaipen@gmail.com>
2019-11-08 16:51:28 -0500

    traslate 2

--- src/firebase.js
+++ src/firebase.js
@@ -12,26 +12,7 @@ var firebaseConfig = {
   measurementId: "G-VP6F6HCQTR"
 };
 
-
 const fb = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 export { fb, db }
-
-
-// var firebase = require("firebase/app");
-// require("firebase/auth");
-// require("firebase/firestore")
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBuYbigctH6zGdZlEbgAP1P4iih8XCxPw4",
-//   authDomain: "appshop-b8e43.firebaseapp.com",
-//   databaseURL: "https://appshop-b8e43.firebaseio.com",
-//   projectId: "appshop-b8e43",
-//   storageBucket: "appshop-b8e43.appspot.com",
-//   messagingSenderId: "199409086527",
-//   appId: "1:199409086527:web:7ccfe1fd07d8ed695c888a",
-//   measurementId: "G-VP6F6HCQTR"
-// };
-
-// firebase.initializeApp(firebaseConfig);
-//export firebase
